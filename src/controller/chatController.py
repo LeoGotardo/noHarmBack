9d61798fa1db8b97d390a90c97bf466ab017a9cc
LeoGotardo <leonardo.gotardo2@gmail.com>
2026-02-18 14:10:10 -0300

    Refactor:  - Refactored all the file structure for better organization and mantainability.  - Aplied the espected security measures on the file structure.

--- src/controller/chatController.py
+++
@@ -1,6 +0,0 @@
-from model.chatModel import ChatModel, Chat
-from model.userModel import UserModel, User
-
-class ChatController(Chat):
-    def startCHat(by: User, to: User) -> tuple[int, Chat] | tuple[int, str]:
-        ...
--- src/controller/userController.py
+++
@@ -1,5 +0,0 @@
-from userModel import User
-
-class UserController(User):
-    IS_AUTHQ = False
-    
