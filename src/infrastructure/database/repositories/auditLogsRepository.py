5358af1b1f09481b2f19d06be8c7e545e5cb215b
LeoGotardo <leonardo.gotardo2@gmail.com>
2026-03-25 13:53:19 -0300

    Feat: Started the schematization of the repositories

--- src/infrastructure/database/repositories/auditLogsRepository.py
+++ src/infrastructure/database/repositories/auditLogsRepository.py
@@ -1,5 +1,71 @@
 from domain.entities.user import User
+from domain.entities.auditLogs import AuditLogs
+
+
+from datetime import datetime
+
 
 class AuditLogsRepository(User):
     def __init__(self, db):
         self.db = db
+        
+    
+    def findById(self, id: str) -> AuditLogs:
+        """Find an audit log by ID
+        
+        Args:
+            id (str): AuditLogs ID
+            
+        Returns:
+            AuditLogs: AuditLogs with his full data
+        """
+        ...
+        
+        
+    def findByType(self, type: int) -> list[AuditLogs]:
+        """Find all audit logs by type
+        
+        Args:
+            type (int): AuditLogs type
+            
+        Returns:
+            list[AuditLogs]: List of AuditLogs
+        """
+        ...
+        
+    
+    def findByCatalystId(self, catalyst_id: int) -> list[AuditLogs]:
+        """Find all audit logs by catalyst ID
+        
+        Args:
+            catalyst_id (str): Catalyst ID
+            
+        Returns:
+            list[AuditLogs]: List of AuditLogs
+        """
+        ...
+        
+    
+    def findByDateRange(self, start: datetime, end: datetime) -> list[AuditLogs]:
+        """Find all audit logs by date range
+        
+        Args:
+            start (datetime): Start date
+            end (datetime): End date
+            
+        Returns:
+            list[AuditLogs]: List of AuditLogs
+        """
+        ...
+        
+        
+    def create(self, AuditLogs: AuditLogs) -> AuditLogs:
+        """Create an audit log
+        
+        Args:
+            AuditLogs (AuditLogs): AuditLogs to create
+            
+        Returns:
+            AuditLogs: AuditLogs with his full data
+        """
+        ...
--- src/infrastructure/database/repositories/chatRepository.py
+++ src/infrastructure/database/repositories/chatRepository.py
@@ -1,5 +1,105 @@
 from domain.entities.chat import Chat
 
+from datetime import datetime
+
 class ChatRepository(Chat):
     def __init__(self, db):
         self.db = db
+        
+        
+    def findById(self, id: str) -> Chat:
+        """Find a chat by ID
+        
+        Args:
+            id (str): Chat ID
+            
+        Returns:
+            Chat: Chat with his full data
+        """
+        ...
+        
+    
+    def findByParticipants(self, participant_id: str) -> list[Chat]:
+        """Find all chats by participant ID
+        
+        Args:
+            participant_id (str): Participant ID
+            
+        Returns:
+            list[Chat]: List of Chats
+        """
+        ...
+        
+    
+    def findAllByUserId(self, user_id: str) -> list[Chat]:
+        """Find all chats by user ID
+        
+        Args:
+            user_id (str): User ID
+            
+        Returns:
+            list[Chat]: List of Chats
+        """
+        ...
+        
+        
+    def create(self, Chat: Chat) -> Chat:
+        """Create a chat
+        
+        Args:
+            Chat (Chat): Chat to create
+            
+        Returns:
+            Chat: Chat with his full data
+        """
+        ...
+        
+        
+    def updateStatus(self, id: str, status: int) -> Chat:
+        """Update a chat status
+        
+        Args:
+            id (str): Chat ID
+            status (int): New status
+            
+        Returns:
+            Chat: Chat with his full data
+        """
+        ...
+        
+        
+    def updateEndedAt(self, id: str, ended_at: datetime) -> Chat:
+        """Update a chat ended at
+        
+        Args:
+            id (str): Chat ID
+            ended_at (datetime): New ended at
+            
+        Returns:
+            Chat: Chat with his full data
+        """
+        ...
+        
+        
+    def delete(self, id: str) -> bool:
+        """Delete a chat
+        
+        Args:
+            id (str): Chat ID
+            
+        Returns:
+            bool: True if chat was deleted, False if not
+        """
+        ...
+        
+        
+    def softDelete(self, id: str) -> bool:
+        """Soft delete a chat
+        
+        Args:
+            id (str): Chat ID
+            
+        Returns:
+            bool: True if chat was soft deleted, False if not
+        """
+        ...
