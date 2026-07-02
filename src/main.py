02c3828b804dbcea2fe9d704494e53a91de1289f
LeoGotardo <leonardo.gotardo2@gmail.com>
2026-07-02 15:34:23 -0300

    added middleware

--- src/main.py
+++ src/main.py
@@ -36,10 +36,11 @@ app = FastAPI(
 )
 
 app.state.limiter = limiter
-app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
-app.add_middleware(SecurityHeadersMiddleware)
 app.add_middleware(RateLimitMiddleware)
+app.add_middleware(SecurityHeadersMiddleware)
+
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
